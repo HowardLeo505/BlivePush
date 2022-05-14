b6a7f0b492679c20ace48d66a938704f3104e9c1
HowardLeo505 <1294596801@qq.com>
2022-05-15 02:47:57 +0800

    22051502

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from bilibili_api import live,sync
 import time
+import yaml,os
 import requests
 
-roomid = *
-url = 'https://api2.pushdeer.com/message/push?pushkey=*&text=*'
+#好吧大晚上闲的蛋疼写了个读文件以后直接用配置文件了
+with open(os.path.join(os.path.dirname(__file__),'config.yml'),encoding='UTF-8') as conf:
+    config = yaml.load(conf.read(),Loader = yaml.Loader)
+    room_id = config['roomid']
+    key = config['pushkey']
+    content = config['pushcontent']
 
+roomid = room_id
+url = 'https://api2.pushdeer.com/message/push?pushkey=*&text=*'
 pers = 1
 
 room = live.LiveRoom(roomid)
